16d8175b5580f5449898e00e11ffbd48a557b66b
ibrahimaq <64918426+ibrahimaq@users.noreply.github.com>
2022-08-12 18:06:21 +0100

    fixed gatsby-plugin-manifest icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/favicon.png",
+        icon: "src/assets/images/favicon.png",
       },
     },
     {
