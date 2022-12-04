e30d8dde91b57f09b564351742015ab79457cace
ibrahimaq <ibrahim_aq@hotmail.co.uk>
2022-12-04 14:31:03 +0000

    integrated tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-postcss',
 
     {
       resolve: `gatsby-source-contentful`,
