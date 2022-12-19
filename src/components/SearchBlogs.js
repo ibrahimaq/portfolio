e877510b027c3184215a35bf1a257e5f69509025
ibrahimaq <ibrahim_aq@hotmail.co.uk>
2022-12-19 01:44:03 +0000

    completed contact,form & search blogs

---
+++ src/components/SearchBlogs.js
@@ -0,0 +1,26 @@
+import React from "react"
+import MagnifyingGlass from "../assets/icons/MagnifyingGlass"
+
+const SearchBlogs = ({ searchTerm, setSearchTerm }) => {
+    return (
+        <div>
+            <p>Search for a blog</p>
+            <label
+                htmlFor="search"
+                className="flex flex-row w-full md:w-1/2 bg-transparent border-b  border-l border-font-greydark focus:outline-1 focus:outline-font-greydark p-2 mt-2 mb-5 cursor-pointer"
+            >
+                <MagnifyingGlass customClass='w-6 h-6' />
+                <input
+                    id="search"
+                    type="search"
+                    className="w-full focus:outline-none bg-inherit text-font-greydark pl-3"
+                    placeholder="..."
+                    value={searchTerm || ""}
+                    onChange={e => setSearchTerm(e.target.value)}
+                />
+            </label>
+        </div>
+    )
+}
+
+export default SearchBlogs
--- src/components/cards/AllCards.js
+++ src/components/cards/AllCards.js
@@ -6,25 +6,7 @@ import Card from "./Card";
 
 
 const AllCards = ({data}) => {
-
-
-    // const [isTablet, setIsTablet] = useState<Boolean | null>(null);
-
-    // useEffect(() => {
-    //     const resize = () => {
-    //         if (window.innerWidth > 640 && window.innerWidth < 1280) {
-    //             setIsTablet(true);
-    //         } else {
-    //             setIsTablet(false);
-    //         }
-    //     }
-    //     window.addEventListener('resize', resize);
-        
-
-    //     return () => window.removeEventListener('resize', resize);
-    // }, []);
-
-
+   
 
     return ( 
         // Desktop cards in a row
@@ -38,45 +20,6 @@ const AllCards = ({data}) => {
             })}
         </div>
         }
-        {/* {isTablet && data && 
-            <div className="flex flex-row">
-                <div className="flex flex-col">
-                    <Link to='/'>
-                        <GatsbyImage image={data[0].featuredImage.gatsbyImageData} alt='' />
-                    </Link>
-                    <h3 className="font-bold text-lg text-font-greydark">{data[0].markdown.childMarkdownRemark.frontmatter.title}</h3>
-                    <ul className="flex flex-row flex-wrap gap-2">        
-                        {data[0].markdown.childMarkdownRemark.frontmatter.tags.slice(0,3).map((tag: string, i: number) => {
-                            return (
-                            <li key={i} className='ml-1 text-blue text-sm rounded-sm cursor-pointer'>#{tag}</li> 
-                            )
-                        })}
-                    </ul>
-                </div>
-                <ul className="flex flex-col    ">
-                    {data.slice(1,3).map(blog => {
-                        return (
-                            <li key={blog.id} className='flex flex-row'>
-                                <Link to='/' className="flex w-[200px] h-[200px]">
-                                    <GatsbyImage image={blog.featuredImage.gatsbyImageData} alt='' className='w-[200px] h-[200px]' />
-                                </Link>
-                                <div className="">
-                                <h3 className="font-bold text-lg text-font-greydark">{blog.markdown.childMarkdownRemark.frontmatter.title}</h3>
-                                    <ul className="flex flex-row flex-wrap gap-2">
-                                        {blog.markdown.childMarkdownRemark.frontmatter.tags.slice(0,3).map((tag: string, i: number) => {
-                                            return (
-                                                <li key={i} className='ml-1 text-blue text-sm rounded-sm cursor-pointer'>#{tag}</li> 
-                                            )
-                                        })}
-                                    </ul>
-                                </div>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        
-        } */}
         </>
      );
 }
